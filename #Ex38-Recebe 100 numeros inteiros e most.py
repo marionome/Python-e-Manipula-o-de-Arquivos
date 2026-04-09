ee55481054785975a4052153eedcb553aebdc015
mrmario6969 <marioninjaaa@gmail.com>
2026-04-09 00:26:28 -0300

    100 numeros inteiros

---
+++ #Ex38-Recebe 100 numeros inteiros e most.py
@@ -0,0 +1,51 @@
+#Ex38-Recebe 100 numeros inteiros e mostrar o maior e menor
+import os
+def gravar(c,linha_arq):
+    global dir,arq
+    if (os.path.exists(dir) and os.path.isdir(dir)):
+        tipo="w"
+        rota=dir+"/"+arq
+        enc="utf-8"
+        if (os.path.exists(rota) and c > 0):
+            tipo = "a"
+        with open(rota,tipo,encoding=enc) as file:
+            file.write(linha_arq)
+
+def inserir(num,cont,fin):
+    global maior,menor
+    linha = str()
+    if fin == False:
+        linha = f"{num}\n"
+        gravar(cont,linha)
+    if fin == True:
+        linha = f"Maior numero:{maior}\nMenor numero:{menor}"
+        gravar(cont,linha)
+
+def maior_menor():
+    global maior,menor
+    contador = int(0)
+    final = bool(False)
+    for contador in range(10):
+        numero = int(input("insira um numero:"))
+        if (numero > maior and numero>=0):
+            maior = numero
+        if(numero < menor and numero>=0)or(menor==0 and contador==0):
+            menor = numero
+        inserir(numero,contador,final)
+    final = True
+    inserir(numero,contador,final)
+
+def main():
+    global dir
+    global arq
+    global maior, menor
+    maior = int(0) 
+    menor = int(0)
+    dir=r"C:\Users\mario\OneDrive\Área de Trabalho\www"
+    arq="ex38.txt"
+    os.makedirs(dir,exist_ok=True)
+    os.chmod(dir,0o744)
+    maior_menor()
+
+if __name__ == "__main__":
+    main()
---
+++ #ler arquivo ex38.py
@@ -0,0 +1,34 @@
+#ler arquivo ex38.py e pegar qualquer numero divisivel por 5 e registrar-lo
+import os
+def ler_arq(dir,arqu):
+    rota = dir+"/"+arqu
+    new_arq= dir+"/read38.txt"
+    valor = int()
+    c = int(0)
+    if (os.path.exists(dir) and os.path.isdir):
+        with open(rota) as file:
+            for linha in file:
+                
+                linha.strip()
+                if any(c.isalpha() for c in linha):
+                    continue
+                valor = int(linha)
+                if valor%5 == 0:
+                    tipo="w"
+                    enc="utf-8"
+                    if (os.path.exists(rota) and c > 0):
+                        tipo="a"
+                    with open(new_arq,tipo,encoding=enc) as file:
+                        file.write(str(valor)+"\n")
+                    c+=1
+               
+def main():
+    global arq
+    global dir
+    dir = r"C:\Users\mario\OneDrive\Área de Trabalho\www"
+    arq = "ex38.txt"
+    os.chmod(dir,0o744)
+    ler_arq(dir,arq)
+
+if __name__ == "__main__":
+    main()
